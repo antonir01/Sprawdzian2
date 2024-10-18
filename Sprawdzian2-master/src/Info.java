5168b79725b4148eaba9adc75621b5a4fc488801
antonir01 <antektroj@gmail.com>
2024-10-18 11:23:16 +0200

    Update Info.java

--- Sprawdzian2-master/src/Info.java
+++ Sprawdzian2-master/src/Info.java
@@ -1,7 +1,6 @@
 public class Info {
     public static void main(String[] args) {
 
-        System.out.println("Instrukcja");  // usuń tą linię kodu
         System.out.println("Info dla użytkownika");
 //        na branchu 'twoje imię' popraw następna linijkę dopisując nazwę miejscowości w której mieszkasz
         System.out.println("Jestem uczniem Zespołu Szkół im. ks. Stanisława Staszica w Tarnobrzegu. Mieszkam w ");
