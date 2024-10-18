42e24408d28dcd6b344c354b8c63195618617fd9
antonir01 <antektroj@gmail.com>
2024-10-18 11:41:39 +0200

    Update Info.java

--- Sprawdzian2-master/src/Info.java
+++ Sprawdzian2-master/src/Info.java
@@ -3,8 +3,8 @@ public class Info {
 
         System.out.println("Info dla użytkownika");
 //        na branchu 'twoje imię' popraw następna linijkę dopisując nazwę miejscowości w której mieszkasz
-        System.out.println("Jestem uczniem Zespołu Szkół im. ks. Stanisława Staszica w Tarnobrzegu. Mieszkam w ");
+        System.out.println("Jestem uczniem Zespołu Szkół im. ks. Stanisława Staszica w Tarnobrzegu. Mieszkam w Tarnobrzegu");
 //        na branchu 'twoje imię' w następnej linii dopisz w jaki sposób dochodzisz/dojeżdżasz do szkoły
-
+        System.out.println("Dojeżdzam autem rodziców lub rowerem, ewentualnie na nogach");
     }
 }
