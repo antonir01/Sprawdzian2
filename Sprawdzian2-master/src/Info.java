193ab02c31936469a0e31b548084ef6204ae0fa4
antonir01 <antektroj@gmail.com>
2024-10-18 11:43:05 +0200

    Update Info.java

--- Sprawdzian2-master/src/Info.java
+++ Sprawdzian2-master/src/Info.java
@@ -6,5 +6,6 @@ public class Info {
         System.out.println("Jestem uczniem Zespołu Szkół im. ks. Stanisława Staszica w Tarnobrzegu. Mieszkam w Tarnobrzegu");
 //        na branchu 'twoje imię' w następnej linii dopisz w jaki sposób dochodzisz/dojeżdżasz do szkoły
         System.out.println("Dojeżdzam autem rodziców lub rowerem, ewentualnie na nogach");
+        System.out.println("Po zakończeniu szkoły chciałbym zaciągnąć się do wojska");
     }
 }
